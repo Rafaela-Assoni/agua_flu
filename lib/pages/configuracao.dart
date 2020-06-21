4228f19fd72be61957e33ebf51d17dfae668c1a2
FabLJ <56180069+FabLJ@users.noreply.github.com>
2020-06-21 18:25:07 -0300

    Update configuracao.dart
    Pagina de configuracoes

--- lib/pages/configuracao.dart
+++ lib/pages/configuracao.dart
@@ -1,1 +1,31 @@
+import 'package:controle_animal/components/menu_inferior.dart';
+import 'package:flutter/material.dart';
+import 'package:controle_animal/model/Animal.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class Config extends StatefulWidget {
+  @override
+  _ConfigState createState() => _ConfigState();
+}
+
+class _ConfigState extends State<Config> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Configurações"),
+      ),
+
+      bottomNavigationBar: MenuInferior(),
+
+      /*floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        tooltip: 'Voltar',
+        child: Icon(Icons.arrow_back),
+      ),*/
+    );
+  }
+}
