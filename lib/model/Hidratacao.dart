071572b2db8458cb07c84e46ba50a48746e8e568
Rafaela Assoni <56172490+Rafaela-Assoni@users.noreply.github.com>
2020-06-22 23:07:40 -0300

    Update Hidratacao.dart

--- lib/model/Hidratacao.dart
+++ lib/model/Hidratacao.dart
@@ -1,1 +1,28 @@
+class Hidratacao {
 
+  String _dia;
+  String _quantidade;
+  String _falta;
+
+  Hidratacao(this._dia, this._quantidade, this._falta);
+
+  String get falta => _falta;
+
+  set falta(String value) {
+    _falta = value;
+  }
+
+  String get quantidade => _quantidade;
+
+  set quantidade(String value) {
+    _quantidade = value;
+  }
+
+  String get dia => _dia;
+
+  set dia(String value) {
+    _dia = value;
+  }
+
+
+}
