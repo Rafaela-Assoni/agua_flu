f70e70a861e237661567128304aead487cbac577
Rafaela Assoni <56172490+Rafaela-Assoni@users.noreply.github.com>
2020-06-22 22:47:13 -0300

    Update configuracao.dart

--- lib/pages/configuracao.dart
+++ lib/pages/configuracao.dart
@@ -15,19 +15,130 @@ class _ConfigState extends State<Config> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text("Configurações"),
       ),
 
       bottomNavigationBar: MenuInferior(),
 
-      /*floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        tooltip: 'Voltar',
-        child: Icon(Icons.arrow_back),
-      ),*/
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 80, left: 80, right: 80),
+            child:
+            ButtonBar(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Container(
+                  width: 500,
+                  child: FlatButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(18.0),
+                        side: BorderSide(color: Colors.blue)
+                    ),
+                    child: Text('TROCAR DE SENHA',
+                      style: TextStyle(
+                          fontFamily: 'Oswald',
+                          fontSize: 20,
+                          color: Colors.blue
+                      ),
+                    ),
+                    onPressed: () {
+                      /* */
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 30, left: 80, right: 80),
+            child:
+            ButtonBar(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Container(
+                  width: 500,
+                  child: FlatButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(18.0),
+                        side: BorderSide(color: Colors.blue)
+                    ),
+                    child: Text('MUDAR EMAIL',
+                      style: TextStyle(
+                          fontFamily: 'Oswald',
+                          fontSize: 20,
+                          color: Colors.blue
+                      ),
+                    ),
+                    onPressed: () {
+                      /* */
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 40, left: 80, right: 80),
+            child:
+            ButtonBar(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Container(
+                  width: 500,
+                  child: FlatButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(18.0),
+                        side: BorderSide(color: Colors.blue)
+                    ),
+                    child: Text('EXCLUIR CONTA',
+                      style: TextStyle(
+                          fontFamily: 'Oswald',
+                          fontSize: 20,
+                          color: Colors.blue
+                      ),
+                    ),
+                    onPressed: () {
+                      /* */
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 40, left: 80, right: 80),
+            child:
+            ButtonBar(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Container(
+                  width: 500,
+                  child: FlatButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(18.0),
+                        side: BorderSide(color: Colors.blue)
+                    ),
+                    child: Text('SAIR',
+                      style: TextStyle(
+                          fontFamily: 'Oswald',
+                          fontSize: 20,
+                          color: Colors.blue
+                      ),
+                    ),
+                    onPressed: () {
+                      /* */
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      )
+
     );
   }
 }
